eb9d3b3adf5d87662e5af24d5814071a152e5f7c
Leo Smith <k0oo@icloud.com>
2016-10-19 23:49:26 +0200

    more improvements

--- papimain.h
+++ papimain.h
@@ -3,6 +3,7 @@
 
 #include "papi.h"
 
+// important functions
 int main_loop(struct server_info * inf);
 int connection_handler(int t, struct server_info * inf);
 int commandInterpreter(struct server_info * inf, int * t);
@@ -11,13 +12,13 @@ void bnlisten(struct server_info *);
 void ansetupcon(struct server_info *, int sock);
 void clearbuf(char * buf, int size);
 
+//print functions
 void printFirstScreen(struct server_info *);
 void printPrompt(int s);
 void printAcceptedConnection(struct server_info *, char *);
 
 // Special command functions!
 void help();
-void whoami();
 void changeClient(int * t);
 void download(int sock);
 
