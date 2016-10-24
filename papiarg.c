14fb49f0e3906a3dc75b7f410698b5d388e9f50f
Leo Smith <k0oo@icloud.com>
2016-10-25 01:43:19 +0200

    :peach:

--- papiarg.c
+++ papiarg.c
@@ -7,24 +7,7 @@ void check_arg(struct server_info * inf, char * argv[], int argc)
 {
 	if(argc >= 2){
 
-		if(!strcmp(argv[1], "-usb")){
-
-			if(argc >= 3){
-
-				if(strlen(argv[2]) > 0 && strlen(argv[2]) < 50){
-					printf("usb\n");
-				} else {
-					usage(argv[0]);
-				}
-
-			} else {
-
-				usage(argv[0]);
-				error("Not enought args", 1);
-
-			}
-
-		} else if(!strcmp(argv[1], "-c")){
+		if(!strcmp(argv[1], "-c")){
 			// check for connection and send the notification to the screen and to a file :)
 
 			if(argc >= 3){
@@ -33,17 +16,9 @@ void check_arg(struct server_info * inf, char * argv[], int argc)
 				acandsendnotif(inf, 0);
 			}
 
-		} else if(!strcmp(argv[1], "-gtk")){
-
-			// Gtk invironement with gui interface
-			serverThread.grtk++;
-			printf("IN PROGRESS\n");
-			usage(argv[0]);
-
 		} else if(!strcmp(argv[1], "-v")){
 
 			// Verbose terminal mode no ncurses
-			// nor gtk
 			serverThread.cmd++;
 			printf("IN PROGRESS\n");
 			usage(argv[1]);
