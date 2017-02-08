5184976b7bf8d0732f6b13cc1c88121521629e10
Leo Smith <k0oo@icloud.com>
2017-02-08 23:44:15 +0100

    :peach:

--- server/main.py
+++ server/main.py
@@ -2,11 +2,25 @@
 # -*- coding: utf-8 -*-
 # Script made by papi
 import p4p1
+import sys, os
 from sys import argv
-script, port = argv
 
-print """[*] p4p1 - server listening on 4441
-[*] waiting for client to connect...."""
+def usage():
+	print "./p4p1.py [port]"
+	print " - This program is the server for p4p1.exe"
+	print " - please port forward you're router if you are not using this"
+	print " - on you're local network."
+	sys.exit(1)
+
+try:
+	script, port = argv
+except:
+	usage()
+
+print """\
+[*] p4p1 - server listening on 4441
+[*] waiting for client to connect....\
+"""
 p4p1.usage()
 s = p4p1.server(int(port))
 s.main()
